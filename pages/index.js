e03ce5da2e9541d20382d4fa5f84410069b2de61
slowbro99 <89710915+slowbro99@users.noreply.github.com>
2021-09-01 17:43:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ const useMint = () => {
     // add extra gas if estimate is ok and have more than 1 item
     send(
       amount,
-      amount > 1 && gas
+      amount > 0 && gas
         ? {
             gasLimit: gas.mul(115).div(100),  // add 15% gas to mints to account for out of gas errors
             value: ethTotal
